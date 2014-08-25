01039e1a9cf82763751d40e9a6f5dc84d67215bc
Alex Zawadzki <alexander.zawadzki@gmail.com>
2014-08-25 14:11:39 +0100

    Add keypress function to increase eye separation

--- src/multipass_vtk.cpp
+++ src/multipass_vtk.cpp
@@ -100,6 +100,36 @@ void KeypressCallbackFunction (
 	cd->renderer_l->GetActiveCamera()->Elevation(-1);
     }
 
+    if( 0 == strcmp("bracketleft", (const char*) key)){
+	double camera_l_position[3];
+	double camera_r_position[3];
+	cd->renderer_l->GetActiveCamera()->GetPosition(camera_l_position);
+	cd->renderer_r->GetActiveCamera()->GetPosition(camera_r_position);
+	camera_l_position[0] -= 0.1;
+	camera_r_position[0] += 0.1;
+	cd->renderer_l->GetActiveCamera()->SetPosition(camera_l_position);
+	cd->renderer_r->GetActiveCamera()->SetPosition(camera_r_position);
+
+	cout << "Eye separation is "
+	     << camera_l_position[0] - camera_r_position[0] << endl;
+    }
+
+    if( 0 == strcmp("bracketright", (const char*) key)){
+	double camera_l_position[3];
+	double camera_r_position[3];
+	cd->renderer_l->GetActiveCamera()->GetPosition(camera_l_position);
+	cd->renderer_r->GetActiveCamera()->GetPosition(camera_r_position);
+	if(camera_l_position[0] < camera_r_position[0]){ // don't invert eyes
+	    camera_l_position[0] += 0.1;
+	    camera_r_position[0] -= 0.1;
+	}
+	cd->renderer_l->GetActiveCamera()->SetPosition(camera_l_position);
+	cd->renderer_r->GetActiveCamera()->SetPosition(camera_r_position);
+
+	cout << "Eye separation is "
+	     << camera_l_position[0] - camera_r_position[0] << endl;
+    }
+
 }
 
 class vtkTimerCallback : public vtkCommand{
