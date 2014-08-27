d1c2e2315659eddf0556091a86c037be1bb4b76a
Alex Zawadzki <alexander.zawadzki@gmail.com>
2014-08-27 15:42:54 +0100

    Clean comments

--- src/multipass_vtk.cpp
+++ src/multipass_vtk.cpp
@@ -371,7 +371,7 @@ int main(int argc, char *argv[] )
 
     Rift rift;
 
-// Timed interactor for HMD
+    // Timed interactor for HMD
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
 	vtkSmartPointer<vtkRenderWindowInteractor>::New();
     renderWindowInteractor->SetRenderWindow(renWin);
